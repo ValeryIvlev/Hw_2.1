e2a0889fda6a71a3b312f345618d4844720d18cf
Valery <qaivlev@yandex.ru>
2022-11-10 18:58:22 +0300

    Favorite language changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello ME!");
-        System.out.println("I LOVE JAVA");
+        System.out.println("I LOVE GO");
     }
 }
